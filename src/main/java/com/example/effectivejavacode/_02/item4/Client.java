29df53950b01ecff4bb7c0932668ea610beb6107
jeus1998 <zeus20240228@gmail.com>
2024-10-26 11:57:26 +0900

    [2024:10:26] 이펙티브 자바 02장 아이템4 유틸리티 클래스

---
+++ src/main/java/com/example/effectivejavacode/_02/item4/Client.java
@@ -0,0 +1,7 @@
+package com.example.effectivejavacode._02.item4;
+
+public class Client {
+    public static void main(String[] args) {
+        int sum = UtilClass.sum(10, 20);
+    }
+}
---
+++ src/main/java/com/example/effectivejavacode/_02/item4/UtilClass.java
@@ -0,0 +1,11 @@
+package com.example.effectivejavacode._02.item4;
+
+
+public class UtilClass {
+    private UtilClass() {
+
+    }
+    public static int sum (int a, int b){
+        return a + b;
+    }
+}
