b1440c07723a4c06b283329f5e71e93a3fbc9cd8
jeus1998 <zeus20240228@gmail.com>
2024-11-03 20:58:48 +0900

    [2024:11:03] 이펙티브 자바 03장 아이템15 클래스와 멤버의 접근권한 최소화

---
+++ src/main/java/com/example/effectivejavacode/_03/item15/SomeThing.java
@@ -0,0 +1,11 @@
+package com.example.effectivejavacode._03.item15;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @AllArgsConstructor @ToString
+public class SomeThing {
+    private String item;
+}
---
+++ src/main/java/com/example/effectivejavacode/_03/item15/Thing.java
@@ -0,0 +1,22 @@
+package com.example.effectivejavacode._03.item15;
+
+/**
+ * public 제어자로 final 배열을 반환하면 해당 배열을 직접적으로 변경은 못하지만
+ * 참조하고 있는 값은 외부에서 충분히 변경이 가능하다.
+ * 그러니 private 제어자로 바꾸고 해당 뷰를 반환한다.
+ */
+import java.util.*;
+public class Thing {
+    private static final SomeThing[] PRIVATE_VALUES = {new SomeThing("A"), new SomeThing("B")};
+
+    /**
+     * 근데 이 방식은 매번 PRIVATE_VALUES(배열) -> List -> 불변 리스트
+     * 이렇게 2번의 과정을 거쳐서 반환한다.
+     */
+    public static List<SomeThing> privateValuesAsList(){
+        return Collections.unmodifiableList(Arrays.asList(PRIVATE_VALUES));
+    }
+    public static SomeThing[] getPrivateValues(){
+        return PRIVATE_VALUES.clone();
+    }
+}
